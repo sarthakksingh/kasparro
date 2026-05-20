849ce361eae3ea8b60f56e01c90016d2024672cc
sarthakksingh <sarthaksingh2709@gmail.com>
2026-05-20 20:43:52 +0530

    add add-to-cart action for product answers

--- server/storeData.js
+++ server/storeData.js
@@ -7,4 +7,5 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 export function loadStoreData() {
   const dataPath = path.join(__dirname, "../data/shopify-store.json");
   return JSON.parse(fs.readFileSync(dataPath, "utf8"));
+  
 }
